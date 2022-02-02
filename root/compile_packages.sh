9d39b8a2b55efb1c0c472209ccbcd9115f5f3273
Thorsten Werner <9943685+SamGamdschie@users.noreply.github.com>
2022-02-02 14:29:34 +0100

    Update on Configuration

--- root/compile_packages.sh
+++ root/compile_packages.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 portsnap auto 
-portmaster -addwG -x postfix dovecot dovecot-pigeonhole
-pkg remove -f postfix dovecot dovecot-pigeonhole
-portmaster --packages-build --delete-build-only postfix dovecot dovecot-pigeonhole
+pkg remove -f postfix dovecot dovecot-pigeonhole rspamd
+portmaster --packages-build --delete-build-only postfix dovecot dovecot-pigeonhole rspamd
 portmaster --clean-distfiles
 
 
