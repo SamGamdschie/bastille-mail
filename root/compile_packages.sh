bf4a588c4d6336a5be75fd421ebaa8391e4477a6
Thorsten Werner <9943685+SamGamdschie@users.noreply.github.com>
2022-11-24 20:57:20 +0100

    DCC added, changes to process

--- root/compile_packages.sh
+++ root/compile_packages.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 portsnap auto 
 pkg remove -f postfix dovecot dovecot-pigeonhole rspamd
-portmaster --packages-build --delete-build-only postfix dovecot dovecot-pigeonhole rspamd
+portmaster --packages-build --delete-build-only mail/postfix mail/dovecot mail/dovecot-pigeonhole mail/rspamd mail/dcc-dccd
 portmaster --clean-distfiles
 
 
